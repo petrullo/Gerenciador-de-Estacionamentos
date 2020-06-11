c76c467bcde7962548bc62cc3afea286c4036a5c
petrullo <agpetrullo@gmail.com>
2020-06-11 11:43:45 -0300

    Primeira View

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
-from .models import Pessoa, Veiculo, Marca
+from .models import  (
+    Pessoa,
+    Veiculo,
+    Marca,
+    Parametros,
+    MovRotativo,
+    Mensalista,
+    MovMensalista,
+)
+
+class MovRotativoAdmin(admin.ModelAdmin):
+    list_display = ('checkin', 'checkout', 'pago', 'total', 'horas_total', 'veiculo')
+
+    def veiculo(self, obj):
+        return obj.veiculo
+
+
+class MovMensalistaAdmin(admin.ModelAdmin):
+    list_display = ('mensalista', 'dt_pgto', 'total')
+
+
 
 admin.site.register(Pessoa)
 admin.site.register(Marca)
 admin.site.register(Veiculo)
+admin.site.register(Parametros)
+admin.site.register(Mensalista)
+admin.site.register(MovMensalista, MovMensalistaAdmin)
+admin.site.register(MovRotativo, MovRotativoAdmin)
 
 
