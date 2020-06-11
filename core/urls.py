d8e268c25c648999ac72941d295c4b60ece74c85
petrullo <agpetrullo@gmail.com>
2020-06-11 18:52:59 -0300

    Criadas URLS com todas as listas

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,18 @@
 from django.conf.urls import url, include
-from .views import home
+from .views import (
+    home,
+    lista_pessoas,
+    lista_veiculos,
+    lista_movrotativos,
+    lista_mensalistas,
+    lista_movemensalistas,
+)
 
 urlpatterns = [
-    url('', home, name='core_home'),
+    url(r'^$', home, name='core_home'),
+    url(r'^pessoas/$', lista_pessoas, name='core_lista_pessoas'),
+    url(r'^veiculos/$', lista_veiculos, name='core_lista_veiculos'),
+    url(r'^movrotativos/$', lista_movrotativos, name='core_lista_movrotativos'),
+    url(r'^mensalistas/$', lista_mensalistas, name='core_lista_mensalistas'),
+    url(r'^movemensalistas/$', lista_movemensalistas, name='core_lista_movemensalistas'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import (
+    Pessoa,
+    Veiculo,
+    MovRotativo,
+    Mensalista,
+    MovMensalista,
+)
 
 def home(request):
     context = {'mensagem': 'Olá'}
     return render(request, 'core/index.html', context)
+
+
+def lista_pessoas(request):
+    pessoas = Pessoa.objects.all()
+    return render(request, 'core/lista_pessoas.html',{'pessoas':pessoas})
+
+
+def lista_veiculos(request):
+    veiculos = Veiculo.objects.all()
+    return render(request, 'core/lista_veiculos.html',{'veiculos':veiculos})
+
+
+def lista_movrotativos(request):
+    movrot = MovRotativo.objects.all()
+    return render(request, 'core/lista_movrotativos.html',{'movrot':movrot})
+
+
+def lista_mensalistas(request):
+    mensalista = Mensalista.objects.all()
+    return render(request, 'core/lista_mensalistas.html',{'mensalista':mensalista})
+
+
+def lista_movemensalistas(request):
+    movmensal = MovMensalista.objects.all()
+    return render(request, 'core/lista_movemensalistas.html',{'movmensal':movmensal})
